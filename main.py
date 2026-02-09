e6831b4e85a4f020dbc503afe52434ef50ac8b8b
liuchunyu44 <1416603026@qq.com>
2026-02-09 14:23:15 +0800

    拆分功能

--- main.py
+++ main.py
@@ -22,28 +22,7 @@ from .keyword_trigger import KeywordRoute, KeywordRouter, MatchMode, PermissionL
 from .onebot_api import extract_message_id
 from .waifu_relations import maybe_add_other_half_record
 
-_DEFAULT_KEYWORD_ROUTES: tuple[KeywordRoute, ...] = (
-    KeywordRoute(keyword="今日老婆", action="draw_wife"),
-    KeywordRoute(keyword="抽老婆", action="draw_wife"),
-    KeywordRoute(keyword="我的老婆", action="show_history"),
-    KeywordRoute(keyword="抽取历史", action="show_history"),
-    KeywordRoute(keyword="强娶", action="force_marry"),
-    KeywordRoute(keyword="关系图", action="show_graph"),
-    KeywordRoute(keyword="羁绊图谱", action="show_graph"),
-    KeywordRoute(keyword="rbq排行", action="rbq_ranking"),
-    KeywordRoute(keyword="抽老婆帮助", action="show_help"),
-    KeywordRoute(keyword="老婆插件帮助", action="show_help"),
-    KeywordRoute(
-        keyword="重置记录",
-        action="reset_records",
-        permission=PermissionLevel.ADMIN,
-    ),
-    KeywordRoute(
-        keyword="重置强娶时间",
-        action="reset_force_cd",
-        permission=PermissionLevel.ADMIN,
-    ),
-)
+from .src.constants import _DEFAULT_KEYWORD_ROUTES
 
 class RandomWifePlugin(Star):
     def __init__(self, context: Context, config: AstrBotConfig = None):
